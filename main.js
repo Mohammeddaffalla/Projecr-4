8031a1f068ed7477a8261529deb34b70df48d72b
Mohammeddaffalla <162124005+Mohammeddaffalla@users.noreply.github.com>
2025-12-09 19:58:27 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,27 @@
+let nums = document.querySelectorAll(".num");
+let content = document.querySelector(".content");
+
+let test = false;
+
+
+window.onscroll = ()=> {
+    if(window.screenY = content.offsetTop){
+       if(!test){
+        nums.forEach((e) => {
+    let start  = 0;
+    let end = e.dataset.num;
+
+    let count = setInterval(() =>{
+        start++;
+        e.textContent = start;
+        if(start == end){
+            clearInterval(count);
+        }
+    },2000 / end)
+
+})
+       } 
+       test = true;
+
+    }
+}
